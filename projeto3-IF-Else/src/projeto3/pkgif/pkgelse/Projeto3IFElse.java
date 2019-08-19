d310349636517791a41c33699f3bfc69780ebacd
Joabe5277 <54293241+Joabe5277@users.noreply.github.com>
2019-08-19 20:51:24 -0300

    Add files via upload

---
+++ projeto3-IF-Else/src/projeto3/pkgif/pkgelse/Projeto3IFElse.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto3.pkgif.pkgelse;
+
+/**
+ *
+ * @author aluno
+ */
+public class Projeto3IFElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        if (condicao boleana)
+        */
+        int idade =30;
+        if idade <18)
+            System.out.println("Menor idade:");
+        {
+           else
+        }
+        System.out.println("Maior idade!");
+    }
+    //operadores logicos
+    //E &&(conjucao)
+    // OU || (disjuncao)
+    boolean amigoDoDono = true;
+    if (idade < 18&& amigoDoDono == false){
+        System.out.println("Menor idade. Entrada Proibida.");
+}
+    else {
+        System.out.println("Maior idade. Pode entrar.");
+    
+}
+    //operador de negacao(!)
+    if (idade < 18 && !amigoDoDono){
+        System.out.println("Menor idade. Entrada proibida.");
+}   
+else{ 
+        System.out.println("Maior idade.Pode entrar.");
+}
+    // operador de igualdade (==)
+    int mes =1;
+    if (mes ==1){
+        System.out.println("Janeiro");
+}
+    else
+    {
+    System.out.println("Tempo de Trabalhar!");
+}
